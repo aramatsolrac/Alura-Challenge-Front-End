7d40556c960682b7f90d96178274a06f382e9d6b
aramatsolrac <tamaraaselau@gmail.com>
2021-12-18 14:10:37 -0700

    Add exit button to hamburguer menu function

--- script/menu.js
+++ script/menu.js
@@ -1,13 +1,22 @@
 //hamburguer menu
 const navMenu = document.querySelector(".nav-menu");
 const hamburguerBtn = document.querySelector(".header__mobile__menu");
+const barMenu = document.getElementById("bar-menu");
+const xMenu = document.getElementById("close-menu");
+let showMenu = false;
 
 hamburguerBtn.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    navMenu.classList.remove("active");
-}
+    if (!showMenu) {
+        navMenu.classList.toggle("active");
+        barMenu.style.display = "none";
+        xMenu.style.display = "flex";
+        showMenu = true;
+    } else {
+        navMenu.classList.remove("active");
+        xMenu.style.display = "none";
+        barMenu.style.display = "flex";
+        showMenu = false;
+    }
+};
