6cc6a9b24e665c1122efe3b25608d500dc8be656
aramatsolrac <tamaraaselau@gmail.com>
2021-12-18 12:06:40 -0700

    Populate codes from db

--- script/community.js
+++ script/community.js
@@ -1,15 +1,67 @@
-//like button
-var btnLike = document.getElementById("like"),
-    count = 0;
-const span = btnLike.querySelector('span');
-const icon = btnLike.querySelector('i');
+function clickLikeBtn(id) {
+    console.log('clickLikeBtn', id);
+    const btnLike = document.getElementById(`like-${id}`);
+    const likeNumber = document.getElementById(`like-number-${id}`);
+    const iconLike = btnLike.querySelector('i');
+    likeNumber.innerHTML = parseInt(likeNumber.innerHTML) + 1;
+    if (likeNumber.innerHTML != "0") {
+        iconLike.style.color = "red";
+    }
+}
 
-btnLike.addEventListener("click", click);
+fetch('https://alura-challenge-front-end-default-rtdb.firebaseio.com/codes.json')
+    .then(response => response.json())
+    .then(data => addCard(data));
 
+function addCard(data) {
+    const projectContainer = document.getElementById("container_codes")
 
-function click() {
-    console.log('click')
-    count += 1;
-    span.innerHTML = count;
-    icon.style.color = "red";
-};
+    data.forEach((codeItem) => {
+        projectContainer.innerHTML = `
+        <div class="container__code-editor">
+            <div class="container__code-editor__project">
+                <div class="container__code-editor__border" style="background-color: var(--${codeItem.borderColor})">
+                    <div class="container__code-editor__text">
+                        <div class="container__code-editor__mac-buttons">
+                            <div class="container__code-editor__mac-buttons__red"></div>
+                            <div class="container__code-editor__mac-buttons__yellow"></div>
+                            <div class="container__code-editor__mac-buttons--green"></div>
+                        </div>
+                        <pre class="code-ident">
+                            <code id="code">${codeItem.code}</code>
+                        </pre>
+                    </div>
+                </div>
+
+                <div class="card-actions">
+                    <div class="container__code-editor__project-details">
+                        <h1>${codeItem.projectName}</h1>
+                        <p>${codeItem.projectDescription}</p>
+                    </div>
+                    <div class="container__code-editor__project-details_metrics">
+                        <div class="metrics-button">
+                            <button class="btn-comment">
+                                <i class="fas fa-comment"></i>
+                                <span>${codeItem.comments.length}</span>
+                            </button>
+                            <button class="btn-heart" id="like-${codeItem.id}" onclick="clickLikeBtn(${codeItem.id})">
+                                <i class="fas fa-heart" id="heart" ${codeItem.likes > 0 ? 'style="color: red"' : null}></i>
+                                <span id="like-number-${codeItem.id}">${codeItem.likes}</span>
+                            </button>
+                        </div>
+
+                        <div>
+                            <button class="btn-profile">
+                            <img src="${codeItem.userImage}" alt="${codeItem.userName}">
+                            <p>${codeItem.userName}</p>
+                        </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        ` + projectContainer.innerHTML
+    })
+
+}
--- script/index.js
+++ script/index.js
@@ -1,17 +1,3 @@
-//hamburguer menu
-const navMenu = document.querySelector(".nav-menu");
-const hamburguerBtn = document.querySelector(".header__mobile__menu");
-
-hamburguerBtn.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    navMenu.classList.remove("active");
-}
-
 //picker color
 const colorPicker = document.getElementById("color");
 const border = document.getElementById("code-border");
